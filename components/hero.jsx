73991129a9cf59b72280c31536e93f25848df98f
bhavishykhandelwal <141579048+bhavishykhandelwal@users.noreply.github.com>
2025-02-13 13:30:09 +0530

    Add files via upload

---
+++ components/hero.jsx
@@ -0,0 +1,68 @@
+"use client";
+
+import React, { useEffect, useRef } from "react";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const HeroSection = () => {
+  const imageRef = useRef(null);
+
+  useEffect(() => {
+    const imageElement = imageRef.current;
+
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const scrollThreshold = 100;
+
+      if (scrollPosition > scrollThreshold) {
+        imageElement.classList.add("scrolled");
+      } else {
+        imageElement.classList.remove("scrolled");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <section className="pt-40 pb-20 px-4">
+      <div className="container mx-auto text-center">
+        <h1 className="text-3xl md:text-6xl lg:text-[105px] pb-6 gradient-title">
+          Smartly Manage Your Finances<br /> with Intelligence
+        </h1>
+        <p className="text-xl text-gray-600 mb-8 max-w-2xl mx-auto">
+        An AI-driven financial platform that empowers you to track, analyze,
+         and optimize your spending with smart, real-time insights.
+        </p>
+        <div className="flex justify-center space-x-4">
+          <Link href="/dashboard">
+            <Button size="lg" className="px-8">
+              Get Started
+            </Button>
+          </Link>
+          <Link href="https://www.youtube.com/@bhavishykhandelwal1747">
+            <Button size="lg" variant="outline" className="px-8">
+              Watch Demo
+            </Button>
+          </Link>
+        </div>
+        <div className="hero-image-wrapper mt-5 md:mt-0">
+          <div ref={imageRef} className="hero-image">
+            <Image
+              src="/banner.jpeg"
+              width={1280}
+              height={720}
+              alt="Dashboard Preview"
+              className="rounded-lg shadow-2xl border mx-auto"
+              priority
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
